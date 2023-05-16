8757dd6a0aff9b399e6ccd79b565c7f3f7ebce42
Derrick Mbarani <derrickmbarani@gmail.com>
2023-05-16 20:01:57 +0300

    Hero UI for home page

--- layouts/search.vue
+++ layouts/search.vue
@@ -2,7 +2,7 @@
     <header class="px-2 lg:px-16 2xl:container 2xl:mx-auto">
         <MyNavBar />
     </header>
-    <div>
+    <div class="text-lg">
         <slot/>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,42 @@
 
 <template>
     <main class="px-2 lg:px-16 2xl:container 2xl:mx-auto">
-        <h1 class="text-6xl">CarePrompt Search</h1>
-        <p>Quickly learn obstetric emergency protocols on the go.</p>
-        <MySecondarySearchInput />
+        <!-- Hero section -->
+        <section class="py-16 md:grid md:grid-cols-2 md:items-center md:gap-x-24 md:py-20 lg:grid-cols-3">
+            <div class="flex flex-col gap-y-8 py-4">
+                <div>
+                    <p class="text-xs">For obstetric emergencies</p>
+                    <h1 class="text-6xl">CarePrompt Search</h1>
+                </div>
+                <div class="grid grid-cols-3 gap-x-4">
+                    <p class="col-span-2">Quickly learn obstetric emergency protocols on the go.</p>
+                    <SocialProof class="lg:hidden"/>
+                </div>
+                <MySecondarySearchInput />
+                <div>
+                    <button class="hidden lg:block lg:btn lg:btn-gray"><Icon name="akar-icons:search" size="24px" color="#313131"/>&nbsp;Search Now</button>
+                </div>
+                <SocialProof class="hidden lg:block" />
+            </div>
+
+            <img class="hidden lg:block"
+                src="~/assets/images/hero-illustration.svg" width="480" height="640" draggable="false"/>
+
+
+            <ul class="flex gap-x-4 md:flex-col md:gap-y-4">
+                <li class="flex flex-col gap-y-2"> 
+                    <h2 class="text-[#838383] text-xl md:text-4xl">Effortless</h2>
+                    <p class="text-sm md:text-lg">Don't make me think, it's an emergency!</p>
+                </li>
+                <li class="flex flex-col gap-y-2">
+                    <h2 class="text-[#838383] text-xl md:text-4xl">Creative</h2>
+                    <p class="text-sm md:text-lg">You don't have all the resources, so let's use what you got.</p>
+                </li>
+                <li class="flex flex-col gap-y-2">
+                    <h2 class="text-[#838383] text-xl md:text-4xl">Trusted</h2>
+                    <p class="text-sm md:text-lg">Peer-reviewed, proven protocols by fellow medical practitioners.</p>
+                </li>
+            </ul>
+        </section>
     </main>
 </template>
