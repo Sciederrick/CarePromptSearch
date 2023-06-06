461c880030847ef15c40eacb0c02bc744e98fdcf
Derrick Mbarani <derrickmbarani@gmail.com>
2023-06-07 01:54:46 +0300

    add alert enter & leave animations to improve UX

--- layouts/search.vue
+++ layouts/search.vue
@@ -6,14 +6,32 @@
 
 <template>
   <header class="px-2 lg:px-16 2xl:container 2xl:mx-auto">
-    <MyAlert
-      :type="alert.type"
-      :msg="alert.msg"
-      class="fixed top-2 inset-x-0 mx-2 mx-auto"
-    />
+    <Transition name="slide-fade">
+      <MyAlert
+        :type="alert.type"
+        :msg="alert.msg"
+        class="fixed top-2 inset-x-0 mx-2 mx-auto"
+      />
+    </Transition>
     <MyNavBar />
   </header>
   <div>
     <slot />
   </div>
 </template>
+
+<style scoped>
+  .slide-fade-enter-active {
+    transition: all 0.3s ease-out;
+  }
+
+  .slide-fade-leave-active {
+    transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+  }
+
+  .slide-fade-enter-from,
+  .slide-fade-leave-to {
+    transform: translateY(-20px);
+    opacity: 0;
+  }
+</style>
